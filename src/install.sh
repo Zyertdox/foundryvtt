61912fa0647a5dbc23725da41d7cf8038b3c47b8
Andrei <7522793+Zyertdox@users.noreply.github.com>
2025-05-16 12:46:44 +0200

    Fix script

--- src/install.sh
+++ src/install.sh
@@ -9,7 +9,7 @@ SOURCE_DIR="/app"
 mkdir -p "$TARGET_DIR"
 
 # Check if the target directory is empty
-if [ ! "$(ls -A $TARGET_DIR)" ]; then
+if [ ! -f "$TARGET_DIR/main.js" ]; then
     echo "Target directory '$TARGET_DIR' is empty. Initializing data..."
     cp -r "$SOURCE_DIR/"* "$TARGET_DIR/"
     echo "Data initialization completed."
